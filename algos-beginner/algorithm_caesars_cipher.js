feba757a6089a7e2b748427e678f74ea29a44a3e
TSMG <taneisha@aob.com.jm>
2016-05-22 16:56:03 -0500

    Solved Caesar's Cipher

--- algos-beginner/algorithm_caesars_cipher.js
+++ algos-beginner/algorithm_caesars_cipher.js
@@ -24,14 +24,28 @@ String.fromCharCode()
 */
 
 function rot13(str) { // LBH QVQ VG!
-	console.log(str.charCodeAt(0));
-	return str;
+	//console.log(str.charCodeAt());
+	var eStr = str.split("");
+	var dec = eStr.map(function (val) {
+		//console.log (val);
+		if (val.charCodeAt() >= 65 && val.charCodeAt() <=77) {
+			//console.log ("Inside IF: ", val, String.fromCharCode(val.charCodeAt()+13));
+			val = String.fromCharCode(val.charCodeAt()+13);
+		} else 
+		if (val.charCodeAt() > 77) {
+			//console.log ("Inside ELSE: ", val);
+			val = String.fromCharCode(val.charCodeAt()-13);
+		} return val;
+	});
+	//console.log(eStr, nStr);
+	//console.log("This is +13: ", String.fromCharCode(str.charCodeAt(0)+13));
+	return dec.join("");
 }
 
 // Change the inputs below to test
-console.log(rot13("SERR PBQR PNZC"));
-/*console.log(rot13("SERR PBQR PNZC")); //returns "FREE CODE CAMP"
+console.log(rot13(";"));
+console.log(rot13("SERR PBQR PNZC")); //returns "FREE CODE CAMP"
 console.log(rot13("SERR CVMMN!")); //returns "FREE PIZZA!"
 console.log(rot13("SERR YBIR?")); //returns "FREE LOVE?"
-console.log(rot13("GUR DHVPX OEBJA QBT WHZCRQ BIRE GUR YNML SBK."));*/
+console.log(rot13("GUR DHVPX OEBJA QBT WHZCRQ BIRE GUR YNML SBK."));
 //returns "THE QUICK BROWN DOG JUMPED OVER THE LAZY FOX."
